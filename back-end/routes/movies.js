bd3b821c5496ef4aa1f80662395186109818fadd
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-02-02 11:51:03 -0500

    Update movies.js

--- back-end/routes/movies.js
+++ back-end/routes/movies.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
 import db from "../db/conn.js";
 
 const router = new Router();
@@ -12,10 +13,26 @@ router.get("/", async (req, res) => {
 
     const movies = await collection.find().toArray();
 
-    const topTen = movies.slice(0, 10);
+    const topTen = movies.slice(0, 20);
 
     res.status(200).json(topTen);
-    
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /:id
+ */
+router.get("/:id", async (req, res) => {
+  try {
+    const collection = await db.collection("movies");
+
+    const query = new ObjectId(req.params.id)
+
+    const movie = await collection.findOne({ _id: query });
+
+    res.status(200).json(movie);
   } catch (error) {
     console.log(error);
   }
